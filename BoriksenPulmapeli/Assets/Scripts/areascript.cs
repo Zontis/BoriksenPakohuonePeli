3ab7758120f09b4e583d8a6762c8ef54bd46fed1
atteoholm <71486241+atteoholm@users.noreply.github.com>
2021-02-07 13:31:31 +0200

    a

---
+++ BoriksenPulmapeli/Assets/Scripts/areascript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class areascript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Item on the ground")]
+    public string itemName;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+            
+            other.gameObject.GetComponent<Inventory>().AddItemToInventory(itemName);
+        }
+    }
+}
